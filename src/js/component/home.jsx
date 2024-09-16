016c39a1edc042eae9547cd1b00af433a2c2671c
kymchenet <149273045+kymchenet@users.noreply.github.com>
2024-09-16 22:05:49 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [inputValue, setInputValue] = useState([])	
+	const [tasks, setTasks] = useState([])
+
+	const handlePress = (e) => {
+		if(e.key === "Enter" && inputValue.trim() ) {
+			setTasks([...tasks, inputValue])
+			setInputValue('');
+		}
+	}
+	return ( 
+		<div className="container">
+			<h1>Todos</h1>
+				<input
+					type= "text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={inputValue}
+					placeholder="What will you do today?"
+					onKeyDown={handlePress}
+			></input>
+			<ul> 
+				{tasks.length === 0 ? (
+					<li>No tasks, add tasks</li>
+				) : (
+					 tasks.map((task, i) => (
+					 
+					 <div key={i}>{task}</div>)
+						
+					 )
+				)}
+			</ul>
 		</div>
-	);
+	)
 };
 
 export default Home;
